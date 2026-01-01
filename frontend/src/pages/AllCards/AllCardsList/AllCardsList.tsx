d0a6da11ef4e5bf69012af66f7f7d4983a81e633
alexM421 <alexandremillet421@gmail.com>
2026-01-01 19:56:00 +0100

    Fixed filteredFlashcards Button not displaying

--- frontend/src/pages/AllCards/AllCardsList/AllCardsList.tsx
+++ frontend/src/pages/AllCards/AllCardsList/AllCardsList.tsx
@@ -9,18 +9,14 @@ export default function AllCardsList ({ flashcards, shuffledFlashcardsIds }: { f
     const [showAll, setShowAll] = useState(false)
 
     
-    let filteredFlashcards = shuffledFlashcardsIds.map((id) => flashcards.find((flashcard) => flashcard.id === id)).filter((flashcard) => flashcard !== undefined)
-
-    
-
-    if(!showAll) {
-        filteredFlashcards = filteredFlashcards.slice(0, 12)
-    }
+    const filteredFlashcards = shuffledFlashcardsIds.map((id) => flashcards.find((flashcard) => flashcard.id === id)).filter((flashcard) => flashcard !== undefined)
 
     return(
         <div className={styles["all-cards-list"]}>
             <div className={styles["all-cards-list-cards"]}>
-                {filteredFlashcards.map((flashcard) => (
+                {showAll ? filteredFlashcards.map((flashcard) => (
+                    <FlashcardCard key={flashcard.id} flashcard={flashcard} />
+                )) : filteredFlashcards.slice(0, 12).map((flashcard) => (
                     <FlashcardCard key={flashcard.id} flashcard={flashcard} />
                 ))}
             </div>
