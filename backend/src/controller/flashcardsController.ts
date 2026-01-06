5019a860197451938df755ddb84834cbfcb7d2a2
alexM421 <alexandremillet421@gmail.com>
2026-01-06 21:57:17 +0100

    Fixed wrong import

--- backend/src/controller/flashcardsController.ts
+++ backend/src/controller/flashcardsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../lib/supabase.js";
 
 // Define the Flashcard type
 export type Flashcard = {
